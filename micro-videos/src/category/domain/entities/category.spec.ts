86fc8615cec5d63fa38b0f4a978c54de4ee34096
Matheus Mortari <matheus.mortari@prudentte.com.br>
2022-12-31 09:44:30 -0300

    improving category tests

---
+++ micro-videos/src/category/domain/entities/category.spec.ts
@@ -0,0 +1,58 @@
+import { Category, CategoryProperties } from "./category";
+import { omit } from "lodash";
+
+describe("Category Unit Tests", () => {
+  test("Constructor of category", () => {
+    let category = new Category({
+      name: "Movie",
+    });
+    let props = omit(category.props, "created_at");
+    expect(props).toStrictEqual({
+      name: "Movie",
+      description: null,
+      is_active: true,
+    });
+    expect(category.props.created_at).toBeInstanceOf(Date);
+
+    category = new Category({
+      name: "Movie",
+      description: "some description",
+      is_active: false,
+    });
+    let created_at = new Date();
+    expect(category.props).toStrictEqual({
+      name: "Movie",
+      description: "some description",
+      is_active: false,
+      created_at,
+    });
+
+    category = new Category({
+      name: "Movie",
+      description: "other description",
+    });
+    expect(category.props).toMatchObject({
+      name: "Movie",
+      description: "other description",
+    });
+
+    category = new Category({
+      name: "Movie",
+      is_active: true,
+    });
+    expect(category.props).toMatchObject({
+      name: "Movie",
+      is_active: true,
+    });
+
+    created_at = new Date();
+    category = new Category({
+      name: "Movie",
+      created_at,
+    });
+    expect(category.props).toMatchObject({
+      name: "Movie",
+      created_at,
+    });
+  });
+});
